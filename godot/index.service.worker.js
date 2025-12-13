b5b08faf45cdd67d29b439bc46ebff07f882571d
redeyehare <100tuzi@163.com>
2025-12-13 13:53:04 +0800

    123

--- godot/index.service.worker.js
+++ godot/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765602767|45325430';
+const CACHE_VERSION = '1765605119|2396870686';
 /** @type {string} */
 const CACHE_PREFIX = 'test-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
