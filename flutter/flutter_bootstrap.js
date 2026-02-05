b744aa06abf37b2b7b50c78c25509aeb355c156d
redeyehare <100tuzi@163.com>
2026-02-05 10:27:54 +0800

    123

--- flutter/flutter_bootstrap.js
+++ flutter/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1947970963"
+    serviceWorkerVersion: "3727056204"
   }
 });
